715969dc4d87ee0ec4794ac0b837ee122e55cc32
atamaiNia <bimatideka@gmail.com>
2022-11-12 15:30:35 +0200

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,11 @@
-import { Component } from 'react';
 import 'components/Form/Form.css';
 import SignupForm from 'components/Form/Form';
 
-class App extends Component {
-  formSubmitHandler = data => {
-    console.log(data);
-  };
-
-  render() {
-    const { formSubmitHandler } = this;
-
-    return (
-      <div className="container">
-        <h1>React - Singupform</h1>
-        <SignupForm onSubmitForm={formSubmitHandler} />;
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="container">
+      <h1>React - Singupform</h1>
+      <SignupForm />;
+    </div>
+  );
 }
-
-export default App;
